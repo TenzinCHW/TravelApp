ff924d759d7d617a08701c5b07cb402a06436d7e
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-11-20 00:27:46 +0800

    Added hotel name and budget to bundle to be used in Search.class

--- app/src/main/java/com/meow/hanwei/travelapp/Itinerary.java
+++ app/src/main/java/com/meow/hanwei/travelapp/Itinerary.java
@@ -11,16 +11,14 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
-import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class Itinerary extends AppCompatActivity {
     // KEY: AIzaSyAx3Pj6JFDGef_UyhbiONNnAzCEtxlAuSQ
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +26,13 @@ public class Itinerary extends AppCompatActivity {
 //        Bukit Timah Nature Reserve
         String origin = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&key=AIzaSyAx3Pj6JFDGef_UyhbiONNnAzCEtxlAuSQ";
 
+        database data = new database();
         try {
-            final ArrayList<String> foods = new ArrayList<>();
+            final ArrayList<String> foods = data.getMuseums();
             foods.add("meow");
-            URL url = new URL(origin);
-            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-            foods.add(getContent(connection));
+//            URL url = new URL(origin);
+//            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+//            foods.add(getContent(connection));
             final ArrayAdapter<String> itemsAdapter =
                     new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foods);
             ListView listView = (ListView) findViewById(R.id.itinerary);
@@ -57,26 +56,8 @@ public class Itinerary extends AppCompatActivity {
 
     }
 
-    private String getContent(HttpsURLConnection con){
-        if(con!=null){
-            String content = "";
-            try {
-                BufferedReader br =
-                        new BufferedReader(
-                                new InputStreamReader(con.getInputStream()));
-
-                String input;
-
-                while ((input = br.readLine()) != null){
-                    content += input;
-                }
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return content;
-        }
-        return "Nothing";
-    }
+//    private ArrayList<String> sortLocations(){
+//
+//    }
 
 }
--- app/src/main/java/com/meow/hanwei/travelapp/MainActivity.java
+++ app/src/main/java/com/meow/hanwei/travelapp/MainActivity.java
@@ -4,17 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText hotel = (EditText) findViewById(R.id.hotel);
+    EditText budget = (EditText) findViewById(R.id.budget);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent plan = new Intent(MainActivity.this, Search.class);
                 // Todo: add stuff to the bundle
+                plan.putExtra("Hotel", hotel.getText().toString());
+                plan.putExtra("Budget", budget.getText().toString());
                 startActivity(plan);
             }
         });
