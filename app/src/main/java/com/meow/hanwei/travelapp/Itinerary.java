9652ffd40631d917ed151829d576798dc333ba63
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-11-21 01:31:19 +0800

    Implemented get JSON data and JSON parser

--- app/src/main/java/com/meow/hanwei/travelapp/Itinerary.java
+++ app/src/main/java/com/meow/hanwei/travelapp/Itinerary.java
@@ -7,6 +7,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,45 +15,28 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class Itinerary extends AppCompatActivity {
-    // KEY: AIzaSyDKl5Kpec3loPgTSW9hpU6R5in2ojl3RB8
-
-    ArrayList<String> rawUserLocations;
-    //        Bukit Timah Nature Reserve
-    String origin = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&key=AIzaSyDKl5Kpec3loPgTSW9hpU6R5in2ojl3RB8";
-    ASyncHttps request = new ASyncHttps();
-
+    private String KEY = "AIzaSyDKl5Kpec3loPgTSW9hpU6R5in2ojl3RB8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary);
 
-
         Bundle extras = getIntent().getExtras();  // use this to get the locations the user input
         // TODO: set rawUserLocations to the Arraylist in bundle
         database data = new database();
 
 
-        final ArrayList<String> foods = data.getMuseums();
-        foods.add("meow");
+        final ArrayList<String> places = data.getMuseums();
         final ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foods);
-        try{
-            String addData = request.execute(origin).get();
-            foods.add(addData);
-            itemsAdapter.notifyDataSetChanged();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e){
-            e.printStackTrace();
-        }
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, places);
         ListView listView = (ListView) findViewById(R.id.itinerary);
         listView.setAdapter(itemsAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                foods.remove(i);
+                places.remove(i);
                 itemsAdapter.notifyDataSetChanged();
             }
         });
@@ -62,8 +46,52 @@ public class Itinerary extends AppCompatActivity {
         // TODO: read the distance data
     }
 
-//    private ArrayList<String> sortLocations(){
-//
-//    }
+    private String getJSONString(String fromPlace, ArrayList<String> toPlaces) {
+        String request = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + fromPlace.replace(" ", "+") + "&destinations=";
+        int count = 0;
+        while (count < toPlaces.size()) {
+            request += toPlaces.get(count).replace(" ", "+");
+            if (count < toPlaces.size() - 1) {
+                request += "|";
+            }
+        }
+        request += "&mode=walking&key=" + KEY;
+        ASyncHttps getStuff = new ASyncHttps();
+        String result;
+        try {
+            result = getStuff.execute(request).get();
+            return result;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return "FAILED";
+    }
+
+    private ArrayList<Integer> getDistFromJSON(String time2parse) throws ArrayStoreException {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (time2parse.equals("FAILED")) {
+            throw new ArrayStoreException("NOTHING FOUND GG");
+        } else {
+            try {
+                JSONObject originalData = new JSONObject(time2parse);
+                JSONArray rowsData = (JSONArray) originalData.get("rows");
+                JSONArray elementData;
+                JSONObject distData;
+                Integer dist;
+
+                for (int i = 0; i < rowsData.length(); i++) {
+                    elementData = (JSONArray) rowsData.get(i);
+                    distData = (JSONObject) elementData.get(1);
+                    dist = distData.getInt("value");
+                    result.add(dist);
+                }
+            } catch (JSONException j) {
+                j.printStackTrace();
+            }
+        }
+        return result;
+    }
 
 }
