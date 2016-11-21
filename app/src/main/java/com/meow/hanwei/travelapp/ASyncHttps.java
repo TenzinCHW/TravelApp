e91f70dfdb26e6b510e0d585e7599846c867110a
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-11-21 21:38:43 +0800

    Put all blocking methods into ASyncHttps

--- app/src/main/java/com/meow/hanwei/travelapp/ASyncHttps.java
+++ app/src/main/java/com/meow/hanwei/travelapp/ASyncHttps.java
@@ -2,10 +2,16 @@ package com.meow.hanwei.travelapp;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -16,10 +22,14 @@ public class ASyncHttps extends AsyncTask<String, Void, String> {
     public static final String REQUEST_METHOD = "GET";
     public static final int READ_TIMEOUT = 15000;
     public static final int CONNECTION_TIMEOUT = 15000;
+    private String KEY = "AIzaSyDKl5Kpec3loPgTSW9hpU6R5in2ojl3RB8";
+    private String JSONStr;
+    public ArrayList<Integer> dist;
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+        getDistFromJSON(JSONStr);
     }
 
     @Override
@@ -60,5 +70,52 @@ public class ASyncHttps extends AsyncTask<String, Void, String> {
             result = null;
         }
         return result;
+    }
+
+    public void getJSONString(String fromPlace, ArrayList<String> toPlaces) {
+        String request = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + fromPlace.replace(" ", "+") + "&destinations=";
+        int count = 0;
+        while (count < toPlaces.size()) {
+            request += toPlaces.get(count).replace(" ", "+");
+            if (count < toPlaces.size() - 1) {
+                request += "|";
+            }
+        }
+        request += "&mode=walking&key=" + KEY;
+        ASyncHttps getStuff = new ASyncHttps();
+        String result;
+        try {
+            result = getStuff.execute(request).get();
+            JSONStr = result;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getDistFromJSON(String time2parse) throws ArrayStoreException {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (time2parse.equals("FAILED")) {
+            throw new ArrayStoreException("NOTHING FOUND GG");
+        } else {
+            try {
+                JSONObject originalData = new JSONObject(time2parse);
+                JSONArray rowsData = (JSONArray) originalData.get("rows");
+                JSONArray elementData;
+                JSONObject distData;
+                Integer dist;
+
+                for (int i = 0; i < rowsData.length(); i++) {
+                    elementData = (JSONArray) rowsData.get(i);
+                    distData = (JSONObject) elementData.get(1);
+                    dist = distData.getInt("value");
+                    result.add(dist);
+                }
+            } catch (JSONException j) {
+                j.printStackTrace();
+            }
+        }
+        dist = result;
     }
 }
--- app/src/main/java/com/meow/hanwei/travelapp/Itinerary.java
+++ app/src/main/java/com/meow/hanwei/travelapp/Itinerary.java
@@ -7,15 +7,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
 public class Itinerary extends AppCompatActivity {
-    private String KEY = "AIzaSyDKl5Kpec3loPgTSW9hpU6R5in2ojl3RB8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,53 +39,4 @@ public class Itinerary extends AppCompatActivity {
         // TODO: convert to JSON
         // TODO: read the distance data
     }
-
-    private String getJSONString(String fromPlace, ArrayList<String> toPlaces) {
-        String request = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + fromPlace.replace(" ", "+") + "&destinations=";
-        int count = 0;
-        while (count < toPlaces.size()) {
-            request += toPlaces.get(count).replace(" ", "+");
-            if (count < toPlaces.size() - 1) {
-                request += "|";
-            }
-        }
-        request += "&mode=walking&key=" + KEY;
-        ASyncHttps getStuff = new ASyncHttps();
-        String result;
-        try {
-            result = getStuff.execute(request).get();
-            return result;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        return "FAILED";
-    }
-
-    private ArrayList<Integer> getDistFromJSON(String time2parse) throws ArrayStoreException {
-        ArrayList<Integer> result = new ArrayList<>();
-        if (time2parse.equals("FAILED")) {
-            throw new ArrayStoreException("NOTHING FOUND GG");
-        } else {
-            try {
-                JSONObject originalData = new JSONObject(time2parse);
-                JSONArray rowsData = (JSONArray) originalData.get("rows");
-                JSONArray elementData;
-                JSONObject distData;
-                Integer dist;
-
-                for (int i = 0; i < rowsData.length(); i++) {
-                    elementData = (JSONArray) rowsData.get(i);
-                    distData = (JSONObject) elementData.get(1);
-                    dist = distData.getInt("value");
-                    result.add(dist);
-                }
-            } catch (JSONException j) {
-                j.printStackTrace();
-            }
-        }
-        return result;
-    }
-
 }
